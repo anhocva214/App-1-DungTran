422d8d2fd799ab237971855eafc3c1b6bf6174af
anhocva214 <anhocva214@gmail.com>
2020-09-28 11:18:02 +0700

    1212

--- Components/LoginPage.js
+++ Components/LoginPage.js
@@ -1,36 +1,35 @@
 import { View, Text, Button, Image, TextInput } from 'react-native'
 import React, { useState } from 'react'
 
-const LoginPage = ({ navigation }) => {
 
-    const [_statusPage, set_statusPage] = useState(0);
-    
-    const [_text, set_text] = useState("");
 
-    const SuccessPage = () => {
-        return (
-            <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
-                <View >
-                    <Text style={{ marginBottom: 40, fontSize: 30, textAlign: 'center', color: '#2ac644' }}>N" de matricule correct <Image style={{ width: 30, height: 30 }} source={require('../public/img/img-1.png')} /> </Text>
-                    <Text style={{ fontSize: 20, textAlign: 'center' }}>Benvenue dans i'application</Text>
-                    <Text style={{ fontSize: 20, textAlign: 'center' }}> " Management " </Text>
+const SuccessPage = () => {
+    return (
+        <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
+            <View >
+                <Text style={{ marginBottom: 40, fontSize: 30, textAlign: 'center', color: '#2ac644' }}>N" de matricule correct <Image style={{ width: 30, height: 30 }} source={require('../public/img/img-1.png')} /> </Text>
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>Benvenue dans i'application</Text>
+                <Text style={{ fontSize: 20, textAlign: 'center' }}> " Management " </Text>
 
-                </View>
             </View>
-        )
-    }
+        </View>
+    )
+}
 
-    const ErrorPage = () => {
-        return (
-            <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
-                <View >
-                    <Text style={{ marginBottom: 40, fontSize: 30, textAlign: 'center', color: "#e10f21" }}>N" de matricule incorrect <Image style={{ width: 30, height: 30 }} source={require('../public/img/img-5.png')} /> </Text>
-                    <Text style={{ marginBottom: 40, fontSize: 20, textAlign: 'center' }}>Merci de bien vouloir ressaisir votre numero de matricute</Text>
-                </View>
+const ErrorPage = () => {
+    return (
+        <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
+            <View >
+                <Text style={{ marginBottom: 40, fontSize: 30, textAlign: 'center', color: "#e10f21" }}>N" de matricule incorrect <Image style={{ width: 30, height: 30 }} source={require('../public/img/img-5.png')} /> </Text>
+                <Text style={{ marginBottom: 40, fontSize: 20, textAlign: 'center' }}>Merci de bien vouloir ressaisir votre numero de matricute</Text>
             </View>
-        )
-    }
+        </View>
+    )
+}
 
+
+
+const NoACtionPage = ({_text, set_text, set_statusPage, navigation}) => {
     const handleLogin = () => {
         if (_text.trim() == "1") {
             set_statusPage(1);
@@ -46,25 +45,28 @@ const LoginPage = ({ navigation }) => {
             }, 1000);
         }
     }
+    return (
+        <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
+            <View >
+                <Text style={{ fontSize: 20, textAlign: 'center' }}>Vewillez indiquer</Text>
+                <Text style={{ marginBottom: 40, fontSize: 20, textAlign: 'center' }}>votre n" de matricule</Text>
 
+                <TextInput onChangeText= { (text)=>set_text(text)} defaultValue={_text} style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} />
 
-    const NoACtionPage = () => {
-        return (
-            <View style={{ display: 'flex', justifyContent: 'center', marginTop: 50, flexDirection: 'row', flex: 9 }} >
-                <View >
-                    <Text style={{ fontSize: 20, textAlign: 'center' }}>Vewillez indiquer</Text>
-                    <Text style={{ marginBottom: 40, fontSize: 20, textAlign: 'center' }}>votre n" de matricule</Text>
-
-                    <TextInput onChangeText= { (text)=>set_text(text)} defaultValue={_text} style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} />
-
-                    <View style={{ paddingLeft: 50, paddingRight: 50, marginTop: 20 }}>
-                        <Button title="Enter" color="#e10f21" onPress={() => handleLogin()} />
-                    </View>
+                <View style={{ paddingLeft: 50, paddingRight: 50, marginTop: 20 }}>
+                    <Button title="Enter" color="#e10f21" onPress={() => handleLogin()} />
                 </View>
-
             </View>
-        )
-    }
+
+        </View>
+    )
+}
+
+const LoginPage = ({ navigation }) => {
+
+    const [_statusPage, set_statusPage] = useState(0);
+    
+    const [_text, set_text] = useState("");
 
     const ShowSection = () => {
         if (_statusPage == 2) {
@@ -73,7 +75,7 @@ const LoginPage = ({ navigation }) => {
         else if (_statusPage == 1) {
             return <SuccessPage />
         }
-        return <NoACtionPage />
+        return <NoACtionPage _text={_text} set_text={(text)=>set_text(text)} set_statusPage={(status)=>set_statusPage(status)} navigation={navigation}/>
     }
 
     return (
@@ -84,6 +86,7 @@ const LoginPage = ({ navigation }) => {
                 </View>
                 {ShowSection()}
                 {/* <NoACtionPage /> */}
+                {/* <TextInput onChangeText= { (text)=>set_text(text)} defaultValue={_text} style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }} /> */}
             </View>
         </>
     )
